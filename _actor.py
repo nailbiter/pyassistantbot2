592ab809ad97aac207cd4d88a24deacd07dc663e
Alex Leontiev <nailbiter@dtws-work.in>
2021-12-28 16:30:53 +0900

    stash

--- _actor.py
+++ _actor.py
@@ -20,6 +20,11 @@ ORGANIZATION:
 import _common
 import re
 from datetime import datetime
+import subprocess
+import pymongo
+import _common
+import heartbeat_time
+import os
 
 
 def add_money(text, send_message_cb=None, mongo_client=None):
@@ -53,3 +58,55 @@ def add_money(text, send_message_cb=None, mongo_client=None):
         "amount": amount,
     })
     send_message_cb(f"added amount {amount} to category {category}")
+
+
+def habits(text, send_message_cb=None, **_):
+    cmd = f"python3 heartbeat_habits.py show-habits {text}"
+    ec, out = subprocess.getstatusoutput(cmd)
+    assert ec == 0, (cmd, ec, out)
+    send_message_cb(f"```{out}```", parse_mode="Markdown")
+
+
+_SLEEP_CATS = ["sleeping", "social"]
+
+
+def sleepend(_, send_message_cb=None, mongo_client=None):
+    mongo_coll = mongo_client[_common.MONGO_COLL_NAME]["alex.sleepingtimes"]
+    last_record = mongo_coll.find_one(
+        sort=[("startsleep", pymongo.DESCENDING)])
+    cat = last_record["category"]
+    if _common.get_sleeping_state(mongo_client) is None:
+        send_message_cb("not sleeping")
+        return
+    mongo_coll.update_one(
+        {"startsleep": last_record["startsleep"]}, {"$set": {"endsleep": _common.to_utc_date()}})
+    heartbeat_time.SendKeyboard(
+        mongo_url=os.environ["MONGO_URL"], is_create_bot=False).sanitize_mongo(cat)
+    send_message_cb(
+        f"end sleeping \"{cat}\" (was sleeping {(_now-timedelta(hours=9))-last_record['startsleep']})")
+
+
+def ttask(content, send_message_cb=None, mongo_client=None):
+    mongo_client[_common.MONGO_COLL_NAME]["alex.ttask"].insert_one({
+        "content": content,
+        "date": _common.to_utc_date(),
+    })
+    send_message_cb(f"log \"{content}\"")
+
+
+def sleepstart(cat, send_message_cb=None, mongo_client=None):
+    if cat not in _SLEEP_CATS:
+        send_message_cb(
+            f"cat \"{cat}\" not in \"{','.join(_SLEEP_CATS)}\"")
+        return
+    elif _common.get_sleeping_state(mongo_client) is not None:
+        send_message_cb(f"already sleeping!")
+        return
+    elif mongo_client[_common.MONGO_COLL_NAME]["alex.time"].find_one(sort=[("date", pymongo.DESCENDING)]).get("category", None) is None:
+        send_message_cb(f"waiting for time reply!")
+        return
+
+    mongo_coll = mongo_client[_common.MONGO_COLL_NAME]["alex.sleepingtimes"]
+    mongo_coll.insert_one(
+        {"category": cat, "startsleep": _common.to_utc_date})
+    send_message_cb(f"start sleeping \"{cat}\"")
