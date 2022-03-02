0d25df3cddfc27fac0d44c5cd89c13dd6ae1a4bc
Alex Leontiev <alozz1991@gmail.com>
2022-03-02 18:12:43 +0900

    fixup

--- scripts/slack-gstasks.sh
+++ scripts/slack-gstasks.sh
@@ -7,5 +7,5 @@ rm -rf $FILE
 echo '```'>>$FILE
 ./gstasks.py ls -w EVENING -b today --tag ! -o str>>/tmp/tasks.txt
 echo '```'>>$FILE
-cat /tmp/tasks.txt | jq -Rs '{text:.}'|curl -X POST -H 'Content-type: application/json' --data @- $SLACK_WEBHOOK
+cat /tmp/tasks.txt | /snap/bin/jq -Rs '{text:.}'|curl -X POST -H 'Content-type: application/json' --data @- $SLACK_WEBHOOK
 
