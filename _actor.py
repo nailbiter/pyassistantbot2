ea652158f63b503f4ed5e6fb7884dcb01f5cca35
Alex Leontiev <alozz1991@gmail.com>
2022-04-26 19:04:38 +0900

    stash

--- _actor.py
+++ _actor.py
@@ -121,13 +121,13 @@ def nutrition(text, send_message_cb=None, mongo_client=None):
         2. show amount
     """
     amount, *tail = re.split(r"\s+", text, 1)
-    amount_kcal = _common.simple_math_eval.simple_math_eval(amount)
+    amount_kcal = _common.simple_math_eval.eval_expr(amount)
     mongo_client[_common.MONGO_COLL_NAME]["alex.nutrition"].insert_one({
         "amount_kcal": amount_kcal,
         "tail": None if len(tail) == 0 else tail[0],
         "date": _common.to_utc_datetime(),
     })
-    send_message_cb(f"nutrition \"{(amount,tail)}\"")
+    send_message_cb(f"nutrition \"{(amount_kcal,tail)}\"")
 
 
 def sleepstart(cat, send_message_cb=None, mongo_client=None):
--- _common/simple_math_eval.py
+++ _common/simple_math_eval.py
@@ -10,6 +10,7 @@
 REQUIREMENTS: ---
         BUGS: ---
        NOTES: adapted from https://www.tutorialspoint.com/program-to-evaluate-one-mathematical-expression-without-built-in-functions-in-python
+                and https://stackoverflow.com/a/9558001
       AUTHOR: Alex Leontiev (alozz1991@gmail.com)
 ORGANIZATION:
      VERSION: ---
@@ -18,9 +19,12 @@ ORGANIZATION:
 
 ==============================================================================="""
 #from math import floor, trunc
+import logging
+import ast
+import operator as op
 
 
-def simple_math_eval(s, number_utils=(float, float)):
+def simple_math_eval(s, number_utils=(float, float), is_verbose=False):
     s = list(s[::-1])
     string_to_num, float_to_num = number_utils
 
@@ -49,11 +53,42 @@ def simple_math_eval(s, number_utils=(float, float)):
     ans = get_term()
     while s:
         op, term = s.pop(), get_term()
+        if is_verbose:
+            logging.warning((ans, op, term))
         if op == "+":
             ans += term
         else:
             ans -= term
     return ans
+
+
+# supported operators
+operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
+             ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
+             ast.USub: op.neg}
+
+
+def eval_expr(expr):
+    """
+    >>> eval_expr('2^6')
+    4
+    >>> eval_expr('2**6')
+    64
+    >>> eval_expr('1 + 2*3**(4^5) / (6 + -7)')
+    -5.0
+    """
+    return eval_(ast.parse(expr, mode='eval').body)
+
+
+def eval_(node):
+    if isinstance(node, ast.Num):  # <number>
+        return node.n
+    elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand))
+    else:
+        raise TypeError(node)
 
 
 if __name__ == "__main__":
