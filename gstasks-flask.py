d7f3159204ac504cdea0352d5688810318b919ca
Oleksii Leontiev <alozz1991@gmail.com>
2023-05-04 20:09:29 +0900

    gstasks-flask

--- gstasks-flask.py
+++ gstasks-flask.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """===============================================================================
 
         FILE: /Users/nailbiter/Documents/forgithub/pyassistantbot2/gstasks-flask.py
@@ -23,14 +24,27 @@ import os
 from os import path
 import subprocess
 import uuid
+import logging
 
 app = Flask(__name__)
 
 
-@app.route("/ls",methods=['GET'])
+@app.route("/ls", methods=["GET"])
 def hello_world():
-    cmd = "./gstasks.py ls -o html -b today"
-    return "Hello, World!"
+    gstasks_exe = os.environ.get("GSTASKS_FLASK_GSTASKS_EXE", "./gstasks.py")
+
+    args = request.args
+    keys = " ".join([f"-{k} {v}" for k, v in request.args.to_dict().items()])
+    logging.warning(f"args: {args}")
+
+    out_fn = f"/tmp/{uuid.uuid4()}.html"
+    cmd = f"{gstasks_exe} ls -o html -b today {keys} --out-file {out_fn}"
+    logging.warning(f"cmd: `{cmd}`")
+    ec, out = subprocess.getstatusoutput(cmd)
+    assert ec == 0, (cmd, ec, out)
+    with open(out_fn) as f:
+        out = f.read()
+    return out
 
 
 if __name__ == "__main__":
@@ -41,6 +55,9 @@ if __name__ == "__main__":
     ]
     for env_fn in env_fns:
         if path.isfile(env_fn):
-            LOADED_DOTENV = env_fn
+            # LOADED_DOTENV = env_fn
             load_dotenv(dotenv_path=env_fn)
             break
+
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
