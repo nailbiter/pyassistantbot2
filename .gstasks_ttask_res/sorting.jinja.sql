8b75a92862e2c8d403e967ae45757b40edb56576
Oleksii Leontiev <alozz1991@gmail.com>
2023-08-27 13:54:49 +0900

    gstasks

--- .gstasks_ttask_res/sorting.jinja.sql
+++ .gstasks_ttask_res/sorting.jinja.sql
@@ -1,6 +1,8 @@
-select uuid, 1 as x, udf__md5(uuid) as hash, tagc
+select uuid, 1 as x, udf__md5(uuid) as hash, tagc, is_not_soon
 from (
-     select *, scheduled_date is null as sdin
+     select *,
+     	    scheduled_date is null as sdin,
+	    ifnull((julianday(date(scheduled_date))-julianday(date('{{now}}')) > 2),0) as is_not_soon
      from tasks
 ) left join (
      with t as (
@@ -12,5 +14,5 @@ from (
      from t
      group by uuid
 ) using (uuid)
-order by sdin, scheduled_date asc, tagc desc, _last_modification_date desc 
+order by is_not_soon, sdin, scheduled_date asc, tagc desc, _last_modification_date desc 
 
