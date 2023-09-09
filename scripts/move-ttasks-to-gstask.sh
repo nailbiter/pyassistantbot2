b44e0e0dda8b27e96e8e1e79d07cbd26df820dd3
Oleksii Leontiev <alozz1991@gmail.com>
2023-09-09 18:41:33 +0900

    gstasks/scripts/ttasks-to-gstask

--- scripts/move-ttasks-to-gstask.sh
+++ scripts/move-ttasks-to-gstask.sh
@@ -1,39 +1,11 @@
 #!/bin/sh
 
-# # Tue Aug 29 17:14:29 2023
-# # Tue Aug 29 17:14:13 2023
-# cat (random-fn.py -r .json)|jq '[.[]|{name:.content,label:{mongo_id:.["_id"],date:.date}}]'|./gstasks.py import-file -f- -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind --create-new-tag --dry-run
-# # Tue Aug 29 17:12:33 2023
-# cat (random-fn.py -r .json)|jq '.[0]'
-# # Tue Aug 29 17:11:34 2023
-# cat (random-fn.py -r .json)|jq '[.[]|{name:.content,label:{mongo_id:.["_id"],date:.date}}]'|./gstasks.py import-file -f- -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind --create-new-tag -s today --dry-run
-# # Tue Aug 29 17:02:25 2023
-# cat (random-fn.py -r .json)|jq '.[]|{name:.content,label:{mongo_id:.["_id"],date:.date}}'|./gstasks.py import-file -f- -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind --create-new-tag
-# # Tue Aug 29 17:02:00 2023
-# cat (random-fn.py -r .json)|jq '.[]|{name:.content,label:{mongo_id:.["_id"],date:.date}}'
-# # Tue Aug 29 17:01:39 2023
-# cat (random-fn.py -r .json)|jq '{name:.content,label:{mongo_id:.["_id"],date:.date}}'|./gstasks.py import-file -f- -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind --create-new-tag
-# # Tue Aug 29 17:00:53 2023
-# cat (random-fn.py -r .json)|jq '.'|./gstasks.py import-file -f- -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind --create-new-tag
-# # Tue Aug 29 17:00:46 2023
-# cat (random-fn.py -r .json)|jq '.'|./gstasks.py import-file -f- -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind
-# # Tue Aug 29 16:53:29 2023
-# cat (random-fn.py -r .json)|jq
-# # Tue Aug 29 16:53:21 2023
-# cat (random-fn.py -r .json)
-# # Tue Aug 29 16:52:01 2023
-# # Tue Aug 29 16:51:29 2023
-# ls scripts
-# # Tue Aug 29 16:50:32 2023
-# cat (random-fn.py -r .json)|jq '.[]|.index'|./ttask.py -l- --dry-run
-# # Tue Aug 29 16:50:21 2023
-# cat (random-fn.py -r .json)|jq '.[]|.index'|./ttask.py -l --dry-run
-# # Tue Aug 29 16:49:57 2023
-# cat (random-fn.py -r .json)|jq '.[]|.index'
-# # Tue Aug 29 16:49:44 2023
+echo 'FIXME: may be buggy'
 
-# main
+now=`date "+%Y-%m-%dT%H:%M:%S"`
+echo $now
+
 ./ttask.py -r '#remind' -o json > `random-fn.py .json`
 cat `random-fn.py -r .json`|jq '.[]|.index'|./ttask.py -l-
-# cat (random-fn.py -r .json)|jq '[.[]|{name:.content,label:{mongo_id:.["_id"],date:.date}}]'|./gstasks.py import-file -f- -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind --create-new-tag
-cat `random-fn.py -r .json`|jq '[.[]|{name:.content,label:{mongo_id:.["_id"],date:.date}}]'|./gstasks.py import-file -f- -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind
+cat `random-fn.py -r .json`|jq "[.[]|{name:.content,label:{mongo_id:.[\"_id\"],date:.date,move_date:\"$now\"}}]"> `random-fn.py .json`
+./gstasks.py import-file -f `random-fn.py -r .json` -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind
