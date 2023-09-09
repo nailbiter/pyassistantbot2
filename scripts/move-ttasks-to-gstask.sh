889140afadc10ca9116ef2d26f23043710a19778
Oleksii Leontiev <alozz1991@gmail.com>
2023-09-09 19:42:27 +0900

    gstasks/small

--- scripts/move-ttasks-to-gstask.sh
+++ scripts/move-ttasks-to-gstask.sh
@@ -7,5 +7,5 @@ echo $now
 
 ./ttask.py -r '#remind' -o json > `random-fn.py .json`
 cat `random-fn.py -r .json`|jq '.[]|.index'|./ttask.py -l-
-cat `random-fn.py -r .json`|jq "[.[]|{name:.content,label:{mongo_id:.[\"_id\"],date:.date,move_date:\"$now\"}}]"> `random-fn.py .json`
-./gstasks.py import-file -f `random-fn.py -r .json` -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind -w EVENING
+cat `random-fn.py -r .json`|jq "[.[]|{name:.content,label:{mongo_id:.[\"_id\"],date:.date,move_date:\"$now\"},when:\"EVENING\"}]"> `random-fn.py .json`
+./gstasks.py import-file -f `random-fn.py -r .json` -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind
