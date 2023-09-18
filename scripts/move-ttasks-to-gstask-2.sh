0ae3e132cf6e29f43510c186816c673b99820a15
Oleksii Leontiev <alozz1991@gmail.com>
2023-09-18 19:33:41 +0900

    gstasks/script

--- scripts/move-ttasks-to-gstask-2.sh
+++ scripts/move-ttasks-to-gstask-2.sh
@@ -4,10 +4,7 @@ echo 'FIXME: may be buggy'
 
 now=`date "+%Y-%m-%dT%H:%M:%S"`
 
-./ttask.py -r '#remind' -o json > `random-fn.py .json`
-cat `random-fn.py -r .json` | jq '.[]|.index'|./ttask.py -l-
-cat `random-fn.py -r .json` | jq "[.[]|{name:.content,label:{mongo_id:.[\"_id\"],date:.date,move_date:\"$now\"},when:\"EVENING\"}]"> `random-fn.py .json`
-./gstasks.py import-file -f `random-fn.py -r .json` -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind #error here
+cat `random-fn.py -r .json` | jq "[.[]|{name:.content,label:{mongo_id:.[\"_id\"],date:.date,move_date:\"$now\"},when:\"EVENING\"}]"|./gstasks.py import-file -f-  -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind #error here
 
 echo $now
 
