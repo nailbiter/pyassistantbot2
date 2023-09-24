37a0fdf8e95c4cfe7103c71a4cba9764a1739f3f
Oleksii Leontiev <alozz1991@gmail.com>
2023-09-24 09:03:54 +0900

    stash

--- scripts/gstasks/move-ttasks-to-gstask-1.sh
+++ scripts/gstasks/move-ttasks-to-gstask-1.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo 'FIXME: may be buggy'
-
 ./ttask.py -r '#remind' -o json > `random-fn.py .json`
 cat `random-fn.py -r .json` | jq '.[]|.index'|./ttask.py -l-
--- scripts/gstasks/move-ttasks-to-gstask-2.sh
+++ scripts/gstasks/move-ttasks-to-gstask-2.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo 'FIXME: may be buggy'
-
 now=`date "+%Y-%m-%dT%H:%M:%S"`
 
 cat `random-fn.py -r .json` | jq "[.[]|{name:.content,label:{mongo_id:.[\"_id\"],date:.date,move_date:\"$now\"},when:\"EVENING\"}]"|./gstasks.py import-file -f-  -g $GSTASKS_MISC_TASKS_TAG -g ttask -g remind #error here
@@ -9,4 +7,5 @@ cat `random-fn.py -r .json` | jq "[.[]|{name:.content,label:{mongo_id:.[\"_id\"]
 echo $now
 
 #ls
-./gstasks.py ls -c uuid -g $GSTASKS_MISC_TASKS_TAG -g remind -g ttask -o json -c label -c name 2>/dev/null|jq ".[]|select(.label.move_date==\"$now\")|[.name,.uuid]|@tsv" -r
+./gstasks.py ls -c uuid -g $GSTASKS_MISC_TASKS_TAG -g remind -g ttask -o json -c label -c name 2>/dev/null|jq ".[]|select(.label.move_date==\"$now\")|[.name,.uuid]|@tsv" -r|tee `random-fn.py .tsv`
+echo  `random-fn.py -r .tsv`
