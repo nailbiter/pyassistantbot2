8eb47f2738ab2531f4112f5344daed3930822aaf
Oleksii Leontiev <alozz1991@gmail.com>
2024-01-02 11:19:38 +0100

    stash

---
+++ scripts/gstasks/money-berlin.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""===============================================================================
+
+        FILE: /Users/nailbiter/Documents/forgithub/pyassistantbot2/scripts/gstasks/money-berlin.py
+
+       USAGE: .//Users/nailbiter/Documents/forgithub/pyassistantbot2/scripts/gstasks/money-berlin.py
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2024-01-02T11:19:24.734808
+    REVISION: ---
+
+==============================================================================="""
+
+import click
+#from dotenv import load_dotenv
+import os
+from os import path
+import logging
+
+@click.command()
+def money_berlin():
+    pass
+
+if __name__=="__main__":
+#    fn = ".env"
+#    if path.isfile(fn):
+#        logging.warning(f"loading `{fn}`")
+#        load_dotenv(dotenv_path=fn)
+    money_berlin()
