2a2f19140ae2de500dcf02b507a865cfeae85512
Oleksii Leontiev <alozz1991@gmail.com>
2024-04-13 16:01:59 +0900

    stash

--- produce-docker.py
+++ produce-docker.py
@@ -33,7 +33,7 @@ import natsort
 def copy_dir(dir_: str) -> str:
     files = {
         *glob(f"{dir_}/*.py"),
-        *glob(f"{dir_}/**/*.py", recurrent=True),
+        *glob(f"{dir_}/**/*.py", recursive=True),
     }
     files = natsort.natsorted(files)
     logging.warning(files)
