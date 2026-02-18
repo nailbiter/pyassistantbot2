8f247eadb15a166652b593e04be7e36ae230d173
Oleksii Leontiev <alozz1991@gmail.com>
2026-02-18 08:33:29 -0500

    bugfix

---
+++ .gstasks_ttask_res/styling.jinja.sql
@@ -0,0 +1,9 @@
+select uuid,
+iif(date(scheduled_date)=date('{{now}}'),'is_today','') as class
+ ,scheduled_date
+ ,iif(due is not null,'has_due','') as class_has_due
+ ,iif(status='ENGAGE','has_due','') as class_is_engage
+ ,iif(date(due)<=date('{{now}}'),'is_past_due','') as class_is_past_due
+ ,datetime('{{now}}') as now
+--status as class \n
+from tasks
