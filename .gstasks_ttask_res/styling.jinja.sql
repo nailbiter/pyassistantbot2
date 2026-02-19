76ece63694cac72ce41eea569eb7e442407c1a43
Oleksii Leontiev <alozz1991@gmail.com>
2026-02-18 22:00:15 -0500

    gstasks/styling

--- .gstasks_ttask_res/styling.jinja.sql
+++ .gstasks_ttask_res/styling.jinja.sql
@@ -9,6 +9,7 @@ iif(date(scheduled_date)=date('{{now}}'),'is_today','') as class
  , scheduled_date
  , iif(due is not null,'has_due','') as class_has_due
  , iif(status='ENGAGE','is_engage','') as class_is_engage
+ , iif(status='PENDING','is_pending','') as class_is_pending
  , iif(date(due)<=date('{{now}}'),'is_past_due','') as class_is_past_due
  , datetime('{{now}}') as now
  , iif(t_tags.is_recreational, 'is_recreational', '') class_is_recreational
